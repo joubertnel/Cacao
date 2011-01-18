3c5672c4c2715f71275b426c06979f4b9f1b4a83
Joubert Nel <joubert@joubster.com>
2011-01-18 08:14:36 -0500

    Initial check-in of inspector.

---
+++ CacaoInspectables.h
@@ -0,0 +1,14 @@
+//
+//  CacaoInspectables.h
+//
+//  Created by Joubert Nel on 1/11/11.
+//  Copyright 2011 Infinite Improbability. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CacaoInspectables : NSMutableArray {
+
+}
+
+@end
---
+++ CacaoInspectables.m
@@ -0,0 +1,12 @@
+//
+//  CacaoInspectables.m
+//
+//  Created by Joubert Nel on 1/11/11.
+//  Copyright 2011 Infinite Improbability. All rights reserved.
+//
+
+#import "CacaoInspectables.h"
+
+@implementation CacaoInspectables
+
+@end
